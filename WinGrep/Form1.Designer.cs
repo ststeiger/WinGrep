7bcb84e7b6ef3cb7ad5a5fa41f6914b983cbc576
ststeiger <quandary82@hailmail.net>
2019-10-09 12:19:51 +0200

    Projektdateien hinzufügen.

---
+++ WinGrep/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace WinGrep2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cbJustFiles = new System.Windows.Forms.CheckBox();
+            this.cbIgnoreCase = new System.Windows.Forms.CheckBox();
+            this.cbLineNumbers = new System.Windows.Forms.CheckBox();
+            this.cbRecursive = new System.Windows.Forms.CheckBox();
+            this.lblCurFile = new System.Windows.Forms.Label();
+            this.txtCurFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.lblSearchText = new System.Windows.Forms.Label();
+            this.txtSearchText = new System.Windows.Forms.TextBox();
+            this.lblFiles = new System.Windows.Forms.Label();
+            this.txtFiles = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cbCountLines = new System.Windows.Forms.CheckBox();
+            this.lblDir = new System.Windows.Forms.Label();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.cbEscapeRegex = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // cbJustFiles
+            // 
+            this.cbJustFiles.Location = new System.Drawing.Point(208, 97);
+            this.cbJustFiles.Name = "cbJustFiles";
+            this.cbJustFiles.Size = new System.Drawing.Size(84, 18);
+            this.cbJustFiles.TabIndex = 33;
+            this.cbJustFiles.Text = "Just Files";
+            this.cbJustFiles.CheckedChanged += new System.EventHandler(this.ckJustFiles_CheckedChanged);
+            // 
+            // cbIgnoreCase
+            // 
+            this.cbIgnoreCase.Location = new System.Drawing.Point(104, 119);
+            this.cbIgnoreCase.Name = "cbIgnoreCase";
+            this.cbIgnoreCase.Size = new System.Drawing.Size(96, 18);
+            this.cbIgnoreCase.TabIndex = 32;
+            this.cbIgnoreCase.Text = "Ignore Case";
+            // 
+            // cbLineNumbers
+            // 
+            this.cbLineNumbers.Checked = true;
+            this.cbLineNumbers.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbLineNumbers.Location = new System.Drawing.Point(104, 97);
+            this.cbLineNumbers.Name = "cbLineNumbers";
+            this.cbLineNumbers.Size = new System.Drawing.Size(96, 18);
+            this.cbLineNumbers.TabIndex = 31;
+            this.cbLineNumbers.Text = "Line Numbers";
+            // 
+            // cbRecursive
+            // 
+            this.cbRecursive.Checked = true;
+            this.cbRecursive.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbRecursive.Location = new System.Drawing.Point(14, 97);
+            this.cbRecursive.Name = "cbRecursive";
+            this.cbRecursive.Size = new System.Drawing.Size(84, 18);
+            this.cbRecursive.TabIndex = 30;
+            this.cbRecursive.Text = "Recursive";
+            // 
+            // lblCurFile
+            // 
+            this.lblCurFile.Location = new System.Drawing.Point(12, 149);
+            this.lblCurFile.Name = "lblCurFile";
+            this.lblCurFile.Size = new System.Drawing.Size(84, 12);
+            this.lblCurFile.TabIndex = 28;
+            this.lblCurFile.Text = "Current File";
+            // 
+            // txtCurFile
+            // 
+            this.txtCurFile.BackColor = System.Drawing.SystemColors.Info;
+            this.txtCurFile.Location = new System.Drawing.Point(12, 161);
+            this.txtCurFile.Name = "txtCurFile";
+            this.txtCurFile.ReadOnly = true;
+            this.txtCurFile.Size = new System.Drawing.Size(472, 20);
+            this.txtCurFile.TabIndex = 29;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(456, 21);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(28, 20);
+            this.btnBrowse.TabIndex = 19;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblResults
+            // 
+            this.lblResults.Location = new System.Drawing.Point(12, 189);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(56, 12);
+            this.lblResults.TabIndex = 27;
+            this.lblResults.Text = "Results";
+            // 
+            // txtResults
+            // 
+            this.txtResults.BackColor = System.Drawing.SystemColors.Info;
+            this.txtResults.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtResults.Location = new System.Drawing.Point(12, 201);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResults.Size = new System.Drawing.Size(472, 216);
+            this.txtResults.TabIndex = 26;
+            this.txtResults.WordWrap = false;
+            // 
+            // lblSearchText
+            // 
+            this.lblSearchText.Location = new System.Drawing.Point(204, 53);
+            this.lblSearchText.Name = "lblSearchText";
+            this.lblSearchText.Size = new System.Drawing.Size(196, 12);
+            this.lblSearchText.TabIndex = 23;
+            this.lblSearchText.Text = "Search Pattern (Regular Expression)";
+            // 
+            // txtSearchText
+            // 
+            this.txtSearchText.BackColor = System.Drawing.SystemColors.Window;
+            this.txtSearchText.Location = new System.Drawing.Point(204, 65);
+            this.txtSearchText.Name = "txtSearchText";
+            this.txtSearchText.Size = new System.Drawing.Size(280, 20);
+            this.txtSearchText.TabIndex = 24;
+            this.txtSearchText.TextChanged += new System.EventHandler(this.txtSearchText_TextChanged);
+            this.txtSearchText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearchText_KeyDown);
+            // 
+            // lblFiles
+            // 
+            this.lblFiles.Location = new System.Drawing.Point(12, 53);
+            this.lblFiles.Name = "lblFiles";
+            this.lblFiles.Size = new System.Drawing.Size(84, 12);
+            this.lblFiles.TabIndex = 21;
+            this.lblFiles.Text = "Files";
+            // 
+            // txtFiles
+            // 
+            this.txtFiles.BackColor = System.Drawing.SystemColors.Window;
+            this.txtFiles.Location = new System.Drawing.Point(12, 65);
+            this.txtFiles.Name = "txtFiles";
+            this.txtFiles.Size = new System.Drawing.Size(180, 20);
+            this.txtFiles.TabIndex = 22;
+            this.txtFiles.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtFiles_KeyDown);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Enabled = false;
+            this.btnSearch.Location = new System.Drawing.Point(424, 113);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(60, 24);
+            this.btnSearch.TabIndex = 25;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cbCountLines
+            // 
+            this.cbCountLines.Checked = true;
+            this.cbCountLines.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbCountLines.Location = new System.Drawing.Point(14, 119);
+            this.cbCountLines.Name = "cbCountLines";
+            this.cbCountLines.Size = new System.Drawing.Size(84, 18);
+            this.cbCountLines.TabIndex = 20;
+            this.cbCountLines.Text = "Count Lines";
+            // 
+            // lblDir
+            // 
+            this.lblDir.Location = new System.Drawing.Point(12, 9);
+            this.lblDir.Name = "lblDir";
+            this.lblDir.Size = new System.Drawing.Size(60, 12);
+            this.lblDir.TabIndex = 17;
+            this.lblDir.Text = "Directory";
+            // 
+            // txtDir
+            // 
+            this.txtDir.BackColor = System.Drawing.SystemColors.Window;
+            this.txtDir.Location = new System.Drawing.Point(12, 21);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(436, 20);
+            this.txtDir.TabIndex = 18;
+            this.txtDir.TextChanged += new System.EventHandler(this.txtDir_TextChanged);
+            this.txtDir.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtDir_KeyDown);
+            // 
+            // cbEscapeRegex
+            // 
+            this.cbEscapeRegex.Checked = true;
+            this.cbEscapeRegex.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbEscapeRegex.Location = new System.Drawing.Point(208, 119);
+            this.cbEscapeRegex.Name = "cbEscapeRegex";
+            this.cbEscapeRegex.Size = new System.Drawing.Size(96, 18);
+            this.cbEscapeRegex.TabIndex = 34;
+            this.cbEscapeRegex.Text = "Escape Regex";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 433);
+            this.Controls.Add(this.cbEscapeRegex);
+            this.Controls.Add(this.cbJustFiles);
+            this.Controls.Add(this.cbIgnoreCase);
+            this.Controls.Add(this.cbLineNumbers);
+            this.Controls.Add(this.cbRecursive);
+            this.Controls.Add(this.lblCurFile);
+            this.Controls.Add(this.txtCurFile);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.lblSearchText);
+            this.Controls.Add(this.txtSearchText);
+            this.Controls.Add(this.lblFiles);
+            this.Controls.Add(this.txtFiles);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.cbCountLines);
+            this.Controls.Add(this.lblDir);
+            this.Controls.Add(this.txtDir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WinGrep";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbJustFiles;
+        private System.Windows.Forms.CheckBox cbIgnoreCase;
+        private System.Windows.Forms.CheckBox cbLineNumbers;
+        private System.Windows.Forms.CheckBox cbRecursive;
+        private System.Windows.Forms.Label lblCurFile;
+        private System.Windows.Forms.TextBox txtCurFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Label lblSearchText;
+        private System.Windows.Forms.TextBox txtSearchText;
+        private System.Windows.Forms.Label lblFiles;
+        private System.Windows.Forms.TextBox txtFiles;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.CheckBox cbCountLines;
+        private System.Windows.Forms.Label lblDir;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.CheckBox cbEscapeRegex;
+    }
+}
+
---
+++ WinGrep/Form1.cs
@@ -0,0 +1,399 @@
+﻿
+using System;
+using System.Data;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+
+namespace WinGrep2
+{
+
+
+    public partial class Form1 : Form
+    {
+        protected System.Threading.Thread m_searchthread = null;
+        protected System.Collections.ArrayList m_arrFiles = new System.Collections.ArrayList(); // ArrayList keeping the Files
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pSetControlText = SetControlText;
+            pSwitchCursor = SwitchCursor;
+            //pSetWaitCursor = SetWaitCursor;
+            //pUnsetWaitCursor = UnsetWaitCursor;
+        } // End Constructor
+
+
+        protected void SearchThread()
+        {
+            if (m_searchthread == null)
+            {
+                //Start Searching Thread
+                m_searchthread = new System.Threading.Thread(new System.Threading.ThreadStart(Search));
+                m_searchthread.IsBackground = true;
+                m_searchthread.Start();
+                btnSearch.Text = "Stop";
+            }
+            else //bSearching != null
+            {
+                //Stop Thread
+                m_searchthread.Abort();
+                txtCurFile.Text = "";
+                txtResults.Text = "User Requested Search Abortion!";
+            }
+
+        } // End Sub SearchThread
+
+        protected void txtFiles_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && btnSearch.Enabled == true)
+            {
+                SearchThread();
+            }
+        } // End Sub txtFiles_KeyDown
+
+
+        protected void txtDir_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && btnSearch.Enabled == true)
+            {
+                SearchThread();
+            }
+        } // End Sub txtDir_KeyDown
+
+
+        protected void txtSearchText_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && btnSearch.Enabled == true)
+            {
+                SearchThread();
+            }
+        } // End Sub txtSearchText_KeyDown
+
+
+        //Build the list of Files
+        protected void GetFiles(string strDir, string strExt, bool bRecursive)
+        {
+            //search pattern can include the wild characters '*' and '?'
+            if (strDir.Length > 248)
+                return;
+
+            string[] fileList = System.IO.Directory.GetFiles(strDir, strExt);
+            for (int i = 0; i < fileList.Length; i++)
+            {
+                if (System.IO.File.Exists(fileList[i]))
+                    m_arrFiles.Add(fileList[i]);
+            }
+
+            if (bRecursive == true)
+            {
+                //Get recursively from subdirectories
+                string[] dirList = System.IO.Directory.GetDirectories(strDir);
+                for (int i = 0; i < dirList.Length; i++)
+                {
+                    GetFiles(dirList[i], strExt, true);
+                }
+            } // End if (bRecursive == true)
+
+        } // End Sub GetFiles
+
+
+        public delegate void pSetControlText_t(string strTextBoxName, string strText);
+        public pSetControlText_t pSetControlText;
+
+
+        public void SetControlText(string strButtonName, string strText)
+        {
+            this.Controls[strButtonName].Text= strText;
+        } // End Sub SetControlText
+
+
+        //Search Function
+        protected void Search()
+        {
+            try
+            {
+                string strDir = txtDir.Text;
+                //Check First if the Selected Directory exists
+                if (!System.IO.Directory.Exists(strDir))
+                    MessageBox.Show("Directory doesn't exist!", "Win Grep Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                else
+                {
+                    //SetWaitCursor();
+                    if (this.InvokeRequired)
+                        this.Invoke(pSwitchCursor);
+                    else
+                        pSwitchCursor();
+
+                    if (this.InvokeRequired)
+                        this.Invoke(pSetControlText, new object[] { "txtResults", "" });
+                    else
+                        pSetControlText("txtResults", "");
+
+
+                    //txtResults.Text = "";
+                    //Initialize the Flags
+                    bool bRecursive = cbRecursive.Checked;
+                    bool bIgnoreCase = cbIgnoreCase.Checked;
+                    bool bJustFiles = cbJustFiles.Checked;
+                    bool bEscapeRegex = cbEscapeRegex.Checked;
+
+                    bool bLineNumbers;
+
+                    if (bJustFiles == true)
+                        bLineNumbers = false;
+                    else
+                        bLineNumbers = cbLineNumbers.Checked;
+                    bool bCountLines;
+
+                    if (bJustFiles == true)
+                        bCountLines = false;
+                    else
+                        bCountLines = cbCountLines.Checked;
+
+                    //File Extension
+                    string strExt = txtFiles.Text;
+                    //First empty the list
+                    m_arrFiles.Clear();
+
+                    //Create recursively a list with all the files complying with the criteria
+                    string[] astrExt = strExt.Split(new Char[] { ',' });
+                    for (int i = 0; i < astrExt.Length; i++)
+                    {
+                        //Eliminate white spaces
+                        astrExt[i] = astrExt[i].Trim();
+                        GetFiles(strDir, astrExt[i], bRecursive);
+                    } // Next i
+
+                    //Now all the Files are in the ArrayList, open each one
+                    //iteratively and look for the search string
+                    string strSearch = txtSearchText.Text;
+
+                    if (bEscapeRegex)
+                    {
+                        strSearch = strSearch.Replace("-", "\\-");
+                        strSearch = strSearch.Replace("+", "\\+");
+                        strSearch = System.Text.RegularExpressions.Regex.Escape(strSearch);
+                    }
+                        
+
+                    string strResults = "";
+                    string strLine;
+                    int iLine, iCount;
+                    bool bEmpty = true;
+                    System.Collections.IEnumerator enm = m_arrFiles.GetEnumerator();
+                    while (enm.MoveNext())
+                    {
+                        try
+                        {
+                            if (this.InvokeRequired)
+                                this.Invoke(pSetControlText, new object[] { "txtCurFile", (string)enm.Current });
+                            else
+                                pSetControlText("txtCurFile", (string)enm.Current);
+
+                            //SetTextBoxText("txtCurFile", (string)enm.Current);
+                            //txtCurFile.Text = (string)enm.Current;
+                            System.IO.StreamReader sr = System.IO.File.OpenText((string)enm.Current);
+                            iLine = 0;
+                            iCount = 0;
+                            bool bFirst = true;
+                            while ((strLine = sr.ReadLine()) != null)
+                            {
+                                iLine++;
+                                //Using Regular Expressions as a real Grep
+                                System.Text.RegularExpressions.Match mtch;
+                                if (bIgnoreCase == true)
+                                    mtch = System.Text.RegularExpressions.Regex.Match(strLine, strSearch, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                                else
+                                    mtch = System.Text.RegularExpressions.Regex.Match(strLine, strSearch);
+
+                                if (mtch.Success == true)
+                                {
+                                    bEmpty = false;
+                                    iCount++;
+                                    if (bFirst == true)
+                                    {
+                                        if (bJustFiles == true)
+                                        {
+                                            strResults += (string)enm.Current + "\r\n";
+                                            break;
+                                        }
+                                        else
+                                            strResults += (string)enm.Current + ":\r\n";
+                                        bFirst = false;
+                                    }
+                                    //Add the Line to Results string
+                                    if (bLineNumbers == true)
+                                        strResults += "  " + iLine + ": " + strLine + "\r\n";
+                                    else
+                                        strResults += "  " + strLine + "\r\n";
+                                } // End if (mtch.Success == true)
+                            } // Whend
+                            sr.Close();
+                            if (bFirst == false)
+                            {
+                                if (bCountLines == true)
+                                    strResults += "  " + iCount + " Lines Matched\r\n";
+                                strResults += "\r\n";
+                            } // End if (bFirst == false)
+
+                        } // End try
+                        catch (System.Security.SecurityException)
+                        {
+                            strResults += "\r\n" + (string)enm.Current + ": Security Exception\r\n\r\n";
+                        }
+                        catch (System.IO.FileNotFoundException)
+                        {
+                            strResults += "\r\n" + (string)enm.Current + ": File Not Found Exception\r\n";
+                        }
+
+                    } // Whend
+                    //txtCurFile.Text = "";
+
+                    if (this.InvokeRequired)
+                        this.Invoke(pSetControlText, new object[] { "txtCurFile", "" });
+                    else
+                        pSetControlText("txtCurFile", "");
+
+                    if (bEmpty == true)
+                    {
+                        //txtResults.Text = "No matches found!";
+                        if (this.InvokeRequired)
+                            this.Invoke(pSetControlText, new object[] { "txtResults", "No matches found!" });
+                        else
+                            pSetControlText("txtResults", "No matches found!");
+                    }
+                    else
+                    {
+                        //txtResults.Text = strResults;
+                        if (this.InvokeRequired)
+                            this.Invoke(pSetControlText, new object[] { "txtResults", strResults });
+                        else
+                            pSetControlText("txtResults", strResults);
+                    }
+
+                } // End else of if (!Directory.Exists(strDir))
+
+            } // End Catch
+            finally
+            {
+                m_searchthread = null;
+
+                if (this.InvokeRequired)
+                    this.Invoke(pSwitchCursor);
+                else
+                    pSwitchCursor();
+                //UnsetWaitCursor();
+
+                if (this.InvokeRequired)
+                    this.Invoke(pSetControlText, new object[] { "btnSearch", "Start" });
+                else
+                    pSetControlText("btnSearch", "Start");
+                //btnSearch.Text = "Start";
+
+            } // End Finally
+
+        } // End Sub Search
+
+
+        public delegate void pVoidDelegate_t();
+
+        //public pVoidDelegate_t pSetWaitCursor;
+        //public pVoidDelegate_t pUnsetWaitCursor;
+        public pVoidDelegate_t pSwitchCursor;
+
+
+        public void SwitchCursor()
+        {
+            if (Cursor == Cursors.WaitCursor)
+                Cursor = Cursors.Arrow;
+            else
+                Cursor = Cursors.WaitCursor;
+        } // End Sub SwitchCursor
+
+
+        public void SetWaitCursor()
+        {
+            Cursor = Cursors.WaitCursor;
+        } // End Sub SetWaitCursor
+
+
+        public void UnsetWaitCursor()
+        {
+            if (Cursor == Cursors.WaitCursor)
+                Cursor = Cursors.Arrow;
+        } // End Sub UnsetWaitCursor
+
+
+        protected void btnSearch_Click(object sender, System.EventArgs e)
+        {
+            SearchThread();
+        } // End Sub btnSearch_Click
+
+
+        protected void VerifySearchBtn()
+        {
+            if (txtDir.Text != "" && txtSearchText.Text != "")
+            {
+                btnSearch.Enabled = true;
+            }
+            else
+                btnSearch.Enabled = false;
+        } // End Sub VerifySearchBtn
+
+
+        protected void txtSearchText_TextChanged(object sender, System.EventArgs e)
+        {
+            VerifySearchBtn();
+        } // End Sub txtSearchText_TextChanged
+
+
+        protected void txtDir_TextChanged(object sender, System.EventArgs e)
+        {
+            VerifySearchBtn();
+        } // End Sub txtDir_TextChanged
+
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Select a file";
+            fdlg.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+            fdlg.Filter = "All files (*.*)|*.*";
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                string strFile = fdlg.FileName;
+                //File Extension
+                string strExt;
+                //Get the Directory and file extension
+                txtDir.Text = System.IO.Path.GetDirectoryName(strFile);
+                strExt = System.IO.Path.GetExtension(strFile);
+                txtFiles.Text = "*" + strExt;
+            }
+        } // End Sub btnBrowse_Click
+
+
+        private void ckJustFiles_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbJustFiles.Checked == true)
+            {
+                cbLineNumbers.Enabled = false;
+                cbCountLines.Enabled = false;
+            }
+            else
+            {
+                cbLineNumbers.Enabled = true;
+                cbCountLines.Enabled = true;
+            }
+        } // End Sub ckJustFiles_CheckedChanged
+
+
+    } // End Class Form1 : Form
+
+
+} // End Namespace WinGrep2
